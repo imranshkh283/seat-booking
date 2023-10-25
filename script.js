0f031987ac32e895c03936d91b0bc4aee841bd96
Imran <imranshkh283@gmail.com>
2023-10-25 20:17:20 +0530

    movie theater screen change on dropD

--- script.js
+++ script.js
@@ -3,16 +3,20 @@ const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+const theaterScreen = document.querySelector('.screen');
+const movieDdrop = document.querySelector('movieDropDown');
 
 const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+let movieList = []
+
 const getMovies = async (url) => {
 
     const response = await fetch(url);
-    const data = await response.json();
+    movieList = await response.json();
 
-    data.results.forEach(res => {
+    movieList.results.forEach(res => {
         const optionElement = document.createElement('option');
         optionElement.value = res.id;
         optionElement.innerText = res.title;
@@ -20,6 +24,22 @@ const getMovies = async (url) => {
     });
 }
 
-
 getMovies(APIURL);
 
+const changeMainScreen = (e) => {
+
+    const selectedMovie = movieSelect.value;
+    console.log(selectedMovie);
+    const selectedMovieData = movieList.results.find(ele => ele.id == selectedMovie);
+
+    theaterScreen.style.backgroundImage = `url(${IMGPATH + selectedMovieData.poster_path})`;
+    theaterScreen.style.backgroundSize = 'cover';
+    theaterScreen.style.backgroundPosition = 'center';
+    theaterScreen.style.backgroundRepeat = 'no-repeat';
+    theaterScreen.style.backgroundAttachment = 'fixed';
+    theaterScreen.style.backgroundBlendMode = 'multiply';
+
+}
+
+
+movieSelect.addEventListener('change', changeMainScreen);
